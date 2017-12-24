8578a567d8462c8a5563d2902d6781fcaa977672
rauat <rauat@tuta.io>
2017-12-25 01:07:35 +0530

    Initial commit

---
+++ src/main/java/com/playJs/PlayJsApplication.java
@@ -0,0 +1,12 @@
+package com.playJs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayJsApplication.class, args);
+	}
+}
